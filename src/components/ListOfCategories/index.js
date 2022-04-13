250a65d7e31a4f99410a8b5f2c2b0879b4c38494
evinracher <eevinley@gmail.com>
2022-04-13 11:38:42 -0500

    14: ¿Qué son los Hooks?

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Category } from '../Category';
-import { categories } from '../../../api/db.json';
 import { List, Item } from './styles';
 
+const API_URL = 'https://petgram-server-evinracher.vercel.app';
+
 export function ListOfCategories() {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch(`${API_URL}/categories`)
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
+
   return (
     <List>
       {categories.map((category) => (
