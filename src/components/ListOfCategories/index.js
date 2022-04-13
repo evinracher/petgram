125655ebaf44356d730196bd517aebf6e87d615c
evinracher <eevinley@gmail.com>
2022-04-13 14:53:47 -0500

    15: useEffect: limpiando eventos

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Category } from '../Category';
 import { List, Item } from './styles';
 
@@ -6,6 +6,9 @@ const API_URL = 'https://petgram-server-evinracher.vercel.app';
 
 export function ListOfCategories() {
   const [categories, setCategories] = useState([]);
+  const [showFixed, setShowFixed] = useState(false);
+  const showFixedRef = useRef();
+  showFixedRef.current = showFixed;
 
   useEffect(() => {
     fetch(`${API_URL}/categories`)
@@ -13,13 +16,35 @@ export function ListOfCategories() {
       .then((data) => setCategories(data));
   }, []);
 
-  return (
-    <List>
+  useEffect(() => {
+    const onScroll = () => {
+      const newShowFixed = window.scrollY > 200;
+      // Update state only when needed
+      if (showFixedRef.current !== newShowFixed) {
+        setShowFixed(newShowFixed);
+      }
+    };
+
+    document.addEventListener('scroll', onScroll);
+    return () => {
+      document.removeEventListener('scroll', onScroll);
+    };
+  }, []);
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
       {categories.map((category) => (
         <Item key={category.id}>
           <Category {...category} />
         </Item>
       ))}
     </List>
+  );
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(showFixed)}
+    </>
   );
 }
--- src/components/ListOfCategories/styles.js
+++ src/components/ListOfCategories/styles.js
@@ -7,6 +7,20 @@ export const List = styled.ul`
   &::-webkit-scrollbar {
     display: none;
   }
+  &.fixed {
+    background: #fff;
+    border-radius: 60px;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
+    left: 0;
+    margin: 0 auto;
+    max-width: 400px;
+    padding: 5px;
+    position: fixed;
+    right: 0;
+    top: -20px;
+    transform: scale(0.5);
+    z-index: 1;
+  }
 `;
 
 export const Item = styled.li`
