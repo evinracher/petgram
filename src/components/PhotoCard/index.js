808b6bfd991eaf8d90e3c7a45d0011434c5c292d
evinracher <eevinley@gmail.com>
2022-04-13 16:55:53 -0500

    17: Usando Intersection Observer

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,22 +1,41 @@
-import React from 'react';
-import { ImgWrapper, Button, Img } from './styles';
+import React, { useEffect, useState, useRef } from 'react';
+import { Article, ImgWrapper, Button, Img } from './styles';
 import { MdFavoriteBorder } from 'react-icons/md';
 
 const DEFAULT_IMG =
   'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png';
 
 export function PhotoCard({ id, src = DEFAULT_IMG, likes = 0 }) {
+  const elRef = useRef(null);
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    const observer = new window.IntersectionObserver((entries) => {
+      const { isIntersecting } = entries[0];
+      if (isIntersecting) {
+        setShow(true);
+        observer.disconnect();
+      }
+    });
+
+    observer.observe(elRef.current);
+  }, [elRef]);
+
   return (
-    <article>
-      <a href={`/detail/${id}`}>
-        <ImgWrapper>
-          <Img src={src} alt={src} />
-        </ImgWrapper>
-      </a>
-      <Button>
-        <MdFavoriteBorder size="32px" />
-        {likes} likes
-      </Button>
-    </article>
+    <Article ref={elRef}>
+      {show && (
+        <>
+          <a href={`/detail/${id}`}>
+            <ImgWrapper>
+              <Img src={src} alt={src} />
+            </ImgWrapper>
+          </a>
+          <Button>
+            <MdFavoriteBorder size="32px" />
+            {likes} likes
+          </Button>
+        </>
+      )}
+    </Article>
   );
 }
