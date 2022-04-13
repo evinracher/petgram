e23eb427c5d755fbccf792c41b032b626226c743
evinracher <eevinley@gmail.com>
2022-04-13 09:29:00 -0500

    12: SVGR: de SVG a componente de ReactJS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { ListOfCategories } from './components/ListOfCategories';
 import { ListOfPhotoCards } from './components/ListOfPhotoCards';
+import { Logo } from './components/Logo';
 import { GlobalStyles } from './GlobalStyles';
 
 function App() {
   return (
     <>
       <GlobalStyles />
+      <Logo />
       <ListOfCategories />
       <ListOfPhotoCards />
     </>
